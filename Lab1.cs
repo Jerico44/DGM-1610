733c3b18bb40a1c9cb1f3ee1791d9e2aaa57a186
Eric Menendez <em84057@gmail.com>
2023-05-23 21:53:02 -0600

    Lab1
    Practice

---
+++ Lab1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Lab1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string name = "Eric";
+      Console.WriteLine(name);
+      int number = 20;
+      Console.WriteLine(number);
+    }
+  }
+}
